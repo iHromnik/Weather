3aa9ab2e9bae5e526ccdb3758a95ae7c12031cd2
iHromnik <hromnik@me.com>
2022-04-12 10:46:52 +0300

    add csroll, button,keyboard

---
+++ Weather/LoginViewController.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+override func viewDidLoad() {
+        super.viewDidLoad()
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keybordWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keybordWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+
+        }
+
+         
+    
+    @objc func keybordWillShow (notification: Notification) {
+        guard let kbSize = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+        let insets = UIEdgeInsets(top: 0, left: 0, bottom: kbSize.size.height, right: 0)
+        scrollView.contentInset = insets
+    }
+
+    @objc func keybordWillHide (notification: Notification) {
+        let insets = UIEdgeInsets.zero
+        scrollView.contentInset = insets
+    }
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        print("Hello")
+    }
+    
+}
